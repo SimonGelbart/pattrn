c78c2e5b80d3d7044ad1090bb84ca19da7dceb21
Simon G. <simon.gelbart@efrei.net>
2026-07-10 10:20:40 +0200

    test(routing): cover detailed capture slices

---
+++ tests/Pattrn.Routing.Tests/RouteDetailedSliceTests.cs
@@ -0,0 +1,28 @@
+using static Pattrn.Routing.Tests.TestAssertions;
+
+namespace Pattrn.Routing.Tests;
+
+public sealed class RouteDetailedSliceTests
+{
+    [Test]
+    public void CallerBufferDetailedCapturesReferenceSplitRouteSegments()
+    {
+        var index = PattrnIndex<string, string>
+            .Builder()
+            .AddRoute("/files/{*path}", "handler")
+            .Build();
+        const string path = "/files/a/b.txt";
+        var matches = new PatternMatch<string>[index.GetRouteMatchCountUpperBound(path)];
+        var captures = new PatternCaptureSlice<string>[index.GetRouteCaptureCountUpperBound(path)];
+
+        var matchCount = index.MatchRouteDetailed(path, matches, captures, out var captureCount);
+        var segments = RoutePattern.SplitPath(path);
+
+        ShouldEqual(matchCount, 1);
+        ShouldEqual(captureCount, 1);
+        ShouldEqual(captures[0], new PatternCaptureSlice<string>("path", 1, 2));
+        ShouldSequenceEqual(
+            segments.AsSpan(captures[0].StartSegmentIndex, captures[0].SegmentCount).ToArray(),
+            ["a", "b.txt"]);
+    }
+}
